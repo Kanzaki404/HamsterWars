3059ee19d39afea202b71cbd06e688e8fe87364f
Alexander <Alexander.risteski23@gmail.com>
2020-10-01 21:21:58 +0200

    server deplotment

--- Server/Server.js
+++ Server/Server.js
@@ -18,7 +18,11 @@ var cors = require('cors');
 
 // Then use it before your routes are set up:
 app.use(cors());
-app.use('/', express.static(__dirname + '/../build'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // app.use(express.static('Server'))
 app.use(fileUpload());
 app.use((req, res, next) => {
