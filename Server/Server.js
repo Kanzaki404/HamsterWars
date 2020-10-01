d90a209fdf3c847f251535897a5bcc31d5be41cf
Alexander <Alexander.risteski23@gmail.com>
2020-10-01 23:23:11 +0200

    changes

--- Server/Server.js
+++ Server/Server.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/../build')));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/../build', 'index.html'));
   console.log(__dirname)
 });
 // app.use(express.static('Server'))
